70adfa1073f7619e861c170ba53e2a3ff7a4f78f
Shreyas Prabhu <75876671+ShreyasPrabhu31@users.noreply.github.com>
2026-07-16 16:23:56 -0400

    Add: fruit-into-baskets - submission-0

---
+++ Data Structures & Algorithms/fruit-into-baskets/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        l = 0
+        count = defaultdict(int)
+        maxFruits = 0
+
+        for r in range(len(fruits)):
+            count[fruits[r]] += 1
+
+            while len(count) > 2:
+                count[fruits[l]] -= 1
+                if count[fruits[l]] == 0:
+                    del count[fruits[l]]
+                l += 1
+            maxFruits = max(maxFruits, r - l + 1)
+        return maxFruits
